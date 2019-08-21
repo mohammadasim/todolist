9b4c4630610758a53c77338d412fbf4dae43c0ff
Mohammad Ayub <mohammad.ayub@imagination.com>
2019-08-21 11:22:13 +0100

    added more logic and styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,26 @@
 //Check off specific todos by clicking
-$("li").click(function () {
+// We add events to elements that already exist on page load, so that 
+// it is applied when new sub elements are added.
+$("ul").on("click", "li", function () {
    $(this).toggleClass("completed");
+});
+// click on x to delete to-do
+$("ul").on("click", "span", function (event) {
+   $(this).parent().fadeOut(500, function () {
+      $(this).remove();
+   });
+   event.stopPropagation();
+});
+// Enter text to texbox to generate new to-do on pressing enter
+$("input[type='text']").keypress(function (event) {
+   if (event.which === 13) {
+      // grabbing new todo text from input
+      var todoText = $(this).val();
+      // clear text input
+      $(this).val('');
+      // create a new li and adding to ul
+      $("ul").append("<li><span>X</span> " + todoText + "</li>");
+      
+      
+   }
 });
